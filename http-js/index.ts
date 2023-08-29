336d1df8139e9e080cd4e555120582a774d498df
Alexey Orlenko <alex@aqrln.net>
2023-08-29 22:26:15 +0200

    Implement http-js example

---
+++ http-js/index.ts
@@ -0,0 +1,22 @@
+import Database from "libsql-experimental";
+
+if (!process.env.SQLD_URL) {
+  throw new Error('missing SQLD_URL, run `direnv allow`')
+}
+
+const db = new Database(process.env.SQLD_URL);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS user (
+    id    INTEGER PRIMARY KEY,
+    email TEXT
+  );
+`);
+
+db.exec(`
+  INSERT INTO user (email)
+  VALUES ('user@example.com');
+`);
+
+const result = db.prepare(`SELECT * FROM user`).all();
+console.log(result);
