7a95e6a405c277b33f91270133b0a9f2377b7bea
Alexey Orlenko <alex@aqrln.net>
2023-08-29 16:15:21 +0200

    Set up project structure

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "libsql experiments";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+        nodejs = pkgs.nodejs_latest;
+
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
+
+            nodejs
+            nodejs.pkgs.prettier
+            nodejs.pkgs.ts-node
+            nodejs.pkgs.typescript-language-server
+            nodejs.pkgs.vscode-langservers-extracted
+          ];
+        };
+      }
+    );
+}
