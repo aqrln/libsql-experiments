fe0841ab49058104a8e80e14624fa278fdc89dd1
Alexey Orlenko <alex@aqrln.net>
2023-08-30 10:58:12 +0200

    Add http-rust

--- http-rust/src/main.rs
+++ http-rust/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use libsql::v2::Database;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let url = std::env::var("SQLD_URL")?;
+    let auth_token = std::env::var("SQLD_AUTH_TOKEN")?;
+    let db = Database::open_remote(url, auth_token)?;
+    let conn = db.connect().await?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS user (
+            id    INTEGER PRIMARY KEY,
+            email TEXT
+         );",
+        (),
+    )
+    .await?;
+
+    conn.execute(
+        "INSERT INTO user (email)
+         VALUES ('user@example.com');",
+        (),
+    )
+    .await?;
+
+    let mut result = conn.query("SELECT * FROM user", ()).await?;
+    println!("{}", common::json::to_string(&mut result)?);
+
+    Ok(())
 }
